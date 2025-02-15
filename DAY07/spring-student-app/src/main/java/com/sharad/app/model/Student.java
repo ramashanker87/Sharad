e65ed7579d2015ba1b495ad8c0568f28b08b7fc9
Sharad pareek <80637947+sharad0111@users.noreply.github.com>
2025-02-15 22:35:16 +0530

    day-07

---
+++ DAY07/spring-student-app/src/main/java/com/sharad/app/model/Student.java
@@ -0,0 +1,41 @@
+package com.sharad.app.model;
+
+public class Student {
+
+    String name;
+    int age;
+    String roll;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", roll='" + roll + '\'' +
+                '}';
+    }
+}
